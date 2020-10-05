4c936c98e04d6efcea6f5a54835f37e7df0bc23e
Krishika28 <69198860+Krishika28@users.noreply.github.com>
2020-10-05 15:57:48 +0530

    Update Food.js

--- js/Food.js
+++ js/Food.js
@@ -6,27 +6,27 @@ class food{
     
     }
 
-    updateFoodStock(foodStock){
-        this.foodStock = foodStock;
+    updateFoodStock(FoodStock){
+        this.FoodStock = FoodStock;
     }
 getlastfed(lastFed){
     this.lastFed = lastFed;
 
 }
 deductFood(){
-    if(this.foodStock>0){
-        this.foodStock = this.foodStock-1;
+    if(this.FoodStock>0){
+        this.FoodStock = this.FoodStock-1;
     }
 }
 getfoodstock(){
-    return this.foodStock;
+    return this.FoodStock;
 }
 display(){
    var x = 80, y = 100;
     imageMode(CENTER);
     image(this.image,720,220,70,70);
-if(this.foodStock!=0){
-    for(var i=0; i<this.foodStock; i++){
+if(this.FoodStock!=0){
+    for(var i=0; i<this.FoodStock; i++){
         if(i%10===0){
             x = 80;
             y = y+50; 
